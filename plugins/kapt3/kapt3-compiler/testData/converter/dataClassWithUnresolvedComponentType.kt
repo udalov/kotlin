85d54de710d58e0a6fdf332a30802e3dfeeca95d
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-08-15 16:10:41 +0200

    IR, Fir2Ir: do not generate data class method bodies in kapt mode
     #KT-70718 Fixed

---
+++ plugins/kapt3/kapt3-compiler/testData/converter/dataClassWithUnresolvedComponentType.kt
@@ -0,0 +1,12 @@
+// CORRECT_ERROR_TYPES
+
+@file:Suppress("UNRESOLVED_REFERENCE")
+package test
+
+/*
+typealias TA<W> = U1<W>
+
+data class C(val x: U2, val y: List<U3>, val z: TA<U4>)
+*/
+
+data class C(val x: U2)
